bdb1602f00613063694e3d54359548e2db3b0050
lad475 <lad475@nyu.edu>
2018-04-18 10:31:04 -0400

    Added empty methods with javadocs

--- Board.java
+++ Board.java
@@ -31,4 +31,59 @@ public class Board{
 			}
 		}
 	}
+	/*
+		This method checks to see if the game has completed, whether by the board
+		filling up, or one player winning.  Should the board fill up without a win,
+		this method sets the winner String to "tie".
+
+		@return: a boolean set to true if the game is over, and false if otherwise.
+	*/
+	public boolean gameOver(){
+		if(!this.won() && this.isFull()){
+			this.winner = "tie";
+			return true;
+		}
+		else if(this.won()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	/*
+		This method checks to see if there is a win, and if so, sets the 
+		winner String to either "red" or "black" depending on who wins.
+
+		@return: a boolean set to true if there is a winner, and false otherwise.
+	*/
+	public boolean won(){
+		
+	}
+	/*
+		Checks to see if the 2D board array is fully occupied (no empty String entries)
+
+		@return: true if the board is full, false otherwise
+	*/
+	public boolean isFull(){
+
+	}
+	/*
+		Checks to see if the current column is full
+
+		@return true if the column is full, false otherwise
+	*/
+	public boolean isColFull(int col){
+
+	}
+	/*
+		Adds a token to the column selected by the user if able.
+
+		@params: String color representing the color of the player placing a token,
+		and int col representing the column they wish to put the piece in
+
+		@return a boolean set to true if the add is successful, false otherwise.
+	*/
+	public boolean addToken(String color, int col){
+
+	}
 }
